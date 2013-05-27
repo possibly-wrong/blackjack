e13ec8a53d8e86e01b201e6cb4556b1a612a397b
Eric Farmer <erfarmer@gmail.com>
2013-05-27 18:54:27 -0400

    Modify count.cpp to support TDI strategy with indices

---
+++ src/indices.cpp
@@ -0,0 +1,101 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// indices.cpp
+// Copyright (C) 2013 Eric Farmer (see gpl.txt for details)
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "indices.h"
+
+IndexStrategy::IndexStrategy(BJRules& rules, const double tags[],
+                             const BJShoe& shoe) :
+    BJStrategy(),
+    tags(tags, tags + 11),
+    shoe(shoe) {
+
+    // Compute optimal "infinite" deck strategy, so that strategy depends only
+    // on hand total, not composition.
+    BJShoe infiniteDeck(1024);
+    BJStrategy strategy;
+    BJProgress progress;
+    BJPlayer *tdz = new BJPlayer(infiniteDeck, rules, strategy, progress);
+
+    // "Copy" TDZ strategy into indices[] array, using a representative 2-card
+    // hand for each hard/soft hand total.
+    for (int upCard = 1; upCard <= 10; ++upCard)
+    for (int doubleDown = 0; doubleDown < 2; ++doubleDown)
+    for (int split = 0; split < 2; ++split)
+    for (int surrender = 0; surrender < 2; ++surrender) {
+
+        // Get strategy for hard hands.  For each even count, use a pair hand
+        // just in case splitting is allowed.
+        for (int count = 4; count <= 20; ++count) {
+            if (count % 2 == 0 || split == 0) {
+                BJHand hand;
+                hand.deal(count / 2);
+                hand.deal(count - count / 2);
+                int play = tdz->getOption(hand, upCard,
+                    doubleDown != 0, split != 0, surrender != 0);
+                setIndex(count, false, upCard, doubleDown != 0,
+                    split != 0, surrender != 0, 0, play, play);
+            }
+        }
+
+        // For completeness, a hard 21 is a special case; use 10-9-2.
+        if (split == 0 && surrender == 0) {
+            BJHand hand;
+            hand.deal(10);
+            hand.deal(9);
+            hand.deal(2);
+            if (rules.getDoubleDown(hand) || doubleDown == 0) {
+                int play = tdz->getOption(hand, upCard,
+                    doubleDown != 0, false, false);
+                setIndex(21, false, upCard,
+                    doubleDown != 0, false, false, 0, play, play);
+            }
+        }
+
+        // Get strategy for soft hands.
+        for (int count = 12; count <= 21; ++count) {
+            if (count == 12 || split == 0) {
+                BJHand hand;
+                hand.deal(1);
+                hand.deal(count - 11);
+                int play = tdz->getOption(hand, upCard,
+                    doubleDown != 0, split != 0, surrender != 0);
+                setIndex(count, true, upCard, doubleDown != 0,
+                    split != 0, surrender != 0, 0, play, play);
+            }
+        }
+    }
+    delete tdz;
+}
+
+void IndexStrategy::setIndex(int count, bool soft, int upCard,
+    bool doubleDown, bool split, bool surrender,
+    double trueCount, int play1, int play2) {
+    Index& index = indices[count][soft][upCard][doubleDown][split][surrender];
+    index.trueCount = trueCount;
+    index.play1 = play1;
+    index.play2 = play2;
+}
+
+int IndexStrategy::getOption(const BJHand& hand, int upCard, bool doubleDown,
+    bool split, bool surrender) {
+    Index& index = indices[hand.getCount()][hand.getSoft()][upCard]
+        [doubleDown][split][surrender];
+    return (trueCount(hand, upCard) < index.trueCount ?
+        index.play1 : index.play2);
+}
+
+double IndexStrategy::trueCount(const BJHand& hand, int upCard) {
+
+    // Compute true count, accounting for the dealer up card and cards in the
+    // current hand.  This is a slight approximation in the sense that
+    // post-split hands will not correctly account for all cards seen.
+    double rc = tags[upCard];
+    for (int card = 1; card <= 10; ++card) {
+        rc += tags[card] * (hand.getCards(card) - shoe.getCards(card));
+    }
+    return rc * 52 / (shoe.getCards() - hand.getCards() - 1);
+}
--- src/strategy.cpp
+++ src/strategy.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
 // strategy.cpp
-// Copyright (C) 2012 Eric Farmer (see gpl.txt for details)
+// Copyright (C) 2013 Eric Farmer (see gpl.txt for details)
 //
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -179,8 +179,8 @@ int main() {
 
 // Display title and license notice.
 
-    printf("Blackjack Basic Strategy Calculator version 7.0\n");
-    printf("Copyright (C) 2012 Eric Farmer\n");
+    printf("Blackjack Basic Strategy Calculator version 7.1\n");
+    printf("Copyright (C) 2013 Eric Farmer\n");
     printf("\nThanks to London Colin for many improvements and bug fixes.\n");
     printf("\nThis program comes with ABSOLUTELY NO WARRANTY.\n");
     printf("This is free software, and you are welcome to\n");
